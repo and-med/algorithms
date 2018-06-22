55335dc375284aa2d52cb88ebe2f115904cc4834
Andrii Medytskyi <medandriy@gmail.com>
2018-06-22 19:40:24 +0300

    Startd 489 E

---
+++ CodeForcesTraining/489.E.div2.h
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void doTask(istream& in, ostream& out)
+{
+	int n, q;
+	in >> n >> q;
+	vector<int> a(n);
+	for (int i = 0; i < n; ++i) in >> a[i];
+
+	vector<pair<int, int>> st(2 * n);
+
+
+}
